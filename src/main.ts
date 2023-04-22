fefa281e215c4501e17e443450c13bb552cc0473
sudo <carick.atezong@facsciences-uy1.cm>
2023-04-22 11:10:33 +0100

    Initialisation du projet et installation des outils swagger, typeorm, mysql et  mysql2

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  
+  const config = new DocumentBuilder()
+    .setTitle('Opiol Backend')
+    .setDescription('The Opiol API description')
+    .setVersion('1.0')
+    .addTag('Opiol')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+
+  await app.listen(3000);
+}
+bootstrap();
