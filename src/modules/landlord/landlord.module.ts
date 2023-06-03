d41cdc2275ee319b70d6e360edb0c248df79d314
sudo <carick.atezong@facsciences-uy1.cm>
2023-06-03 16:08:29 +0100

    Ajout de l'entité landlord dans le module landlord

--- src/modules/landlord/landlord.module.ts
+++ src/modules/landlord/landlord.module.ts
@@ -3,9 +3,10 @@ import { LandlordService } from './landlord.service';
 import { LandlordController } from './landlord.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { LandlordEntity } from 'src/entities/Landlord.entity';
+import { PictureEntity } from 'src/entities/Picture.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([LandlordEntity])],
+  imports: [TypeOrmModule.forFeature([LandlordEntity, PictureEntity])],
   controllers: [LandlordController],
   providers: [LandlordService]
 })
